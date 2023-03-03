a3055ed046af3c82b44f857aad56e337b1d5fb14
Andrey Maximenko <andrey.v.maximenko@gmail.com>
2023-03-03 21:05:42 +0300

    Reformat code commit

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Объявляете переменные для входных данных и
-        // параметров программы: начального счёта,
         int startAccount = 100;
-        // суммы пополнения и тп
         int addSum = 1100;
         int totalAccount;
-        // Условным оператором проверяете, превысила ли
-        // сумма пополнения порог, и для этих двух разных
-        // сценариев рассчитываете сумму бонуса и выводите
-        // на экран.
-          if (addSum >1000) {
+        if (addSum > 1000) {
             totalAccount = startAccount + addSum + (addSum / 100);
-          } else {
+        } else {
             totalAccount = startAccount + addSum;
-          }
-        int bonus = totalAccount - startAccount - addSum  ;
+        }
+        int bonus = totalAccount - startAccount - addSum;
         System.out.println("Итоговый счёт: " + totalAccount);
-        System.out.println("Количество бонусных рублей: "+ bonus);
+        System.out.println("Количество бонусных рублей: " + bonus);
     }
 }
